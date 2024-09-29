cfdec1d2fd7e96a3216648b280cb1b7f1180c32e
werinolic <drozdovpavel1997@gmail.com>
2024-09-29 23:14:51 +0300

    update

--- main.tf
+++ main.tf
@@ -1,62 +1,13 @@
 provider "aws" {
-  region = "us-east-1" # Replace with your preferred AWS region
+  region = "eu-north-1"
 }
 
-resource "aws_iam_role" "github_actions_role" {
-  name = "GithubActionsRole"
-
-  # Trust relationship for GitHub Actions via OIDC
-  assume_role_policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Effect : "Allow",
-        Principal : {
-          Federated : "arn:aws:iam::rsschool-devops:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action : "sts:AssumeRoleWithWebIdentity",
-        Condition : {
-          StringEquals : {
-            "token.actions.githubusercontent.com:sub" : "repo:werinolic/rsschool-devops-course-tasks:ref:refs/heads/main"
-          }
-        }
-      }
-    ]
-  })
-}
-
-# Attach required policies to the IAM role
-resource "aws_iam_role_policy_attachment" "ec2_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "route53_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "s3_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "iam_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "vpc_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "sqs_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "eventbridge_full_access" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
+  }
 }
