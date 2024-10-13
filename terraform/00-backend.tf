3ff8a496c155b361964cd1b79135248565ce35d8
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 11:26:17 +0300

    update

--- terraform/00-backend.tf
+++ terraform/00-backend.tf
@@ -2,15 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "rsschool-devops-terraform-state-bucket"
     key            = "terraform/state.tfstate"
-    # dynamodb_table = "terraform-lock-table"
     encrypt        = true
     region         = "eu-west-1"
   }
 }
-
-# resource "aws_s3_bucket" "terraform_state_s3" {
-#   bucket = "rsschool-devops-terraform-state-bucket"
-#   tags = {
-#     name = "TerraformState"
-#   }
-# }
--- terraform/02-oids.tf
+++ terraform/02-oids.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["031267cb861049a0a7ea62e3a5dc781esdwefghj"]
+  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"]
 }
