1293131c05fc21bc5b411d57ba809f905264a9ff
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 13:17:07 +0300

    update

--- terraform/02-oids.tf
+++ terraform/02-oids.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["74F3A68F16524F15424927704C9506F55A9316BD"]
+  thumbprint_list = ["74f3a68f16524f15424927704c9506f55a9316bd"]
 }
