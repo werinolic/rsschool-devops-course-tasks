d417d76138da12910ff3ae4e73b8813ffc5357c3
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 10:07:15 +0300

    update

--- terraform/00-backend.tf
+++ terraform/00-backend.tf
@@ -2,8 +2,15 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket"
     key            = "terraform/state.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
     dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+    region         = "us-east-1"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state_s3" {
+  bucket = "terraform-state-bucket"
+  tags = {
+    name = "TerraformState"
   }
 }
