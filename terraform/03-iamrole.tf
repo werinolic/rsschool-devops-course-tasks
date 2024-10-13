cebf05120573e770a47fba49614129a678739990
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 12:26:42 +0300

    update

--- terraform/03-iamrole.tf
+++ terraform/03-iamrole.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "github_actions_role" {
-  name = "githubactionsrole"
+  name = "GithubActionsRole"
   assume_role_policy = jsonencode({
     Version : "2012-10-17",
     Statement : [
