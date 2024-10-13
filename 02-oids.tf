45736739c4d7a6086229db967b0a3fc4361a297a
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 13:54:58 +0300

    update

--- 02-oids.tf
+++ 02-oids.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   client_id_list  = ["sts.amazonaws.com"]
   url             = "https://token.actions.githubusercontent.com"
-  thumbprint_list = ["74f3a68f16524f15424927704c9506f55a9316bd"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
