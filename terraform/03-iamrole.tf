66a2c83f2e849e87bbb8760caa2b2f255d7bd818
werinolic <drozdovpavel1997@gmail.com>
2024-10-13 11:51:04 +0300

    update

--- terraform/03-iamrole.tf
+++ terraform/03-iamrole.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "github_actions_role" {
         Condition : {
           StringEquals : {
             "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" : "repo:werinolic/rsschool-devops-course-tasks:ref:refs/heads/master"
+            "token.actions.githubusercontent.com:sub" : "repo:werinolic/rsschool-devops-course-tasks:*"
           }
         }
       }
