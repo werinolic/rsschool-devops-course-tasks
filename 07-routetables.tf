e4f04d40752cfa1297d6bec65e2f2783a86c6923
werinolic <drozdovpavel1997@gmail.com>
2024-10-20 18:08:55 +0300

    update router configuration

--- 07-routetables.tf
+++ 07-routetables.tf
@@ -39,9 +39,3 @@ resource "aws_route" "private_nat_gateway" {
   destination_cidr_block = "0.0.0.0/0"
   instance_id            = aws_instance.nat.id
 }
-
-resource "aws_route_table_association" "private" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
-}
